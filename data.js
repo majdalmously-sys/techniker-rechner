a68e8ac512ed0340dd460eea8477aaabed0db590
Majd <majdalmously25@gmail.com>
2026-06-30 21:27:14 +0200

    First version

---
+++ data.js
@@ -0,0 +1,15 @@
+const jobs = [
+  { id: 10060210, name: "Connect Auftrag", price: 14.60 },
+  { id: 10060216, name: "NE5 FTTH Installation Glasfaser", price: 21.00 },
+  { id: 10060222, name: "Connect BULK", price: 6.90 },
+  { id: 10060223, name: "Problembehebung FTTH-Anschluss", price: 22.00 },
+  { id: 10060224, name: "Teilerledigung PB FTTH-AS / Connect Only", price: 10.00 },
+  { id: 10060225, name: "Einbau / Schaltung Gf-Koppler", price: 19.50 },
+  { id: 10060226, name: "VOA FTTH Small", price: 48.50 },
+  { id: 10060227, name: "VOA FTTH Standard", price: 62.50 },
+  { id: 10060228, name: "VOA FTTH Large", price: 80.00 },
+  { id: 10060229, name: "Montage Gf-SP (klassisch)", price: 9.50 },
+  { id: 10060230, name: "Montage Gf-SP (vorkonf.)", price: 7.50 },
+  { id: 10060232, name: "Mini-GWV Basismodul", price: 7.50 },
+  { id: 10060233, name: "Montage Metallhaube", price: 5.80 }
+];
