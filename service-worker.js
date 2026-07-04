d1868990993d9584f2371a36069ad0d38414c726
Majd <majdalmously25@gmail.com>
2026-07-04 13:53:17 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,5 @@
-const CACHE_NAME = "techniker-pro-v2";
+const CACHE_NAME = "techniker-pro-v3";
+
 const FILES = [
   "./",
   "./index.html",
@@ -12,6 +13,8 @@ const FILES = [
 ];
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
+
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(FILES);
@@ -19,10 +22,44 @@ self.addEventListener("install", event => {
   );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+self.addEventListener("activate", event => {
+
+  event.waitUntil(
+    caches.keys().then(keys => {
+
+      return Promise.all(
+        keys.map(key => {
+
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+
+        })
+      );
+
+    }).then(() => self.clients.claim())
   );
+
+});
+
+self.addEventListener("fetch", event => {
+
+  event.respondWith(
+
+    fetch(event.request)
+      .then(response => {
+
+        const copy = response.clone();
+
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, copy);
+        });
+
+        return response;
+
+      })
+      .catch(() => caches.match(event.request))
+
+  );
+
 });
